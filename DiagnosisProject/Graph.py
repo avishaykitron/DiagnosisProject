612b29e435d87855976221ec28038f9beef42879
Ofir Landau <landauof@post.bgu.ac.il>
2019-05-19 18:42:54 +0300

    * Added FIXED example_graph * Added Graph and its init: holds dictionary of comp_id to comp and comp_id to list of ids of comps that are its inputs

--- DiagnosisProject/Graph.py
+++ DiagnosisProject/Graph.py
@@ -15,7 +15,6 @@ class Graph:
             with open(csv_path, 'r') as f:
                 reader = csv.reader(f)
                 content = list(reader)
-                #print(content[0][1:-1])
                 # Add V
                 for count, stringed_comp in enumerate(content[0][1:-1]):
                     # create a component
@@ -26,17 +25,24 @@ class Graph:
                 # Add E (the inputs of each component)
                 for count, stringed_comp in enumerate(content[0][1:-1]):
                     comp_id = count + 1
-                    inputs = ['IN']*len(self.id_to_comp[comp_id].multipliers)   # Empty list for the inputs
+                    inputs = ['SYSIN']*len(self.id_to_comp[comp_id].multipliers)   # Empty list for the inputs, assumes all inputs are SYSINS
                     for col_index, is_input in enumerate(content[comp_id][1:-1]):
                         if int(is_input) > -1:
                             possible_input_index = col_index+1
                             inputs[int(is_input)] = possible_input_index
                     self.id_to_inputs[comp_id] = inputs
-                print(self.id_to_comp)
-                print(self.id_to_inputs)
+
+                for key, value in self.id_to_comp.items():
+                    print("{} : {}".format(key, value.to_string()))
+                for key, value in self.id_to_inputs.items():
+                    print("{} : {}".format(key, value))
 
         except FileNotFoundError:
             print("Unable to read {}".format(csv_path))
+
+    # Run inputs through the graph and returns a list of tuples (comp_id, SYSOUT value) with all SYSOUTs
+    def run_in_graph(self, sysins):
+        pass
 
     # generate valid samples (based on the current graph) with random inputs
     def generate_samples(self, number_of_sample):
@@ -52,4 +58,4 @@ class Graph:
 
 
 example_graph = Graph("DiagnosisProject\\example_graph.csv")
-
+example_graph.run_in_graph([0, 1, 2, 3, 4, 5])
